ce6f257eecde3653e79434dca911ebcc00fed5b8
andres restrepo <arestrepo@vozy.co>
2024-08-09 15:17:06 +0200

    Float 32 converting image

--- ultralytics/data/base.py
+++ ultralytics/data/base.py
@@ -159,7 +159,7 @@ class BaseDataset(Dataset):
                 if extension_image.lower() == 'dcm':
                     # TODO
                     ds = pydicom.dcmread(f, force=True)
-                    im = ds.pixel_array.astype(np.uint8)
+                    im = ds.pixel_array.astype(np.float32) / 255.0
                     im = np.stack([im]*3, -1)
                 else:
                     im = cv2.imread(f)  # BGR
